f8d14a5f9e0fd5368683ccc9c2c972374efa4e8e
Artyom Davydov <fax1ty.ru@gmail.com>
2020-05-22 23:21:21 +0400

    TS fix

---
+++ .example/src/types/index.d.ts
@@ -0,0 +1,10 @@
+import { NativeObject } from 'tabris';
+
+declare global {
+    class SocketIO extends NativeObject {
+        emit(event: string, ...args: any): void;
+        disconnect(): void;
+
+        constructor(url: string);
+    }
+}
