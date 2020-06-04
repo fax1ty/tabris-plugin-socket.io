96e790f3bcbdf082b0882e464fae2fc6f3c29f9b
Artyom Davydov <fax1ty.ru@gmail.com>
2020-06-04 19:22:04 +0400

    js part

--- .example/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { } from 'tabris';
-
-let socket = new SocketIO({ url: 'http://192.168.31.214:3000' });
-socket.emit('test', 'data');
-socket.on('testAnswer', data => {
-    console.log(data);
-});
--- www/SocketIO.js
+++ www/SocketIO.js
@@ -1,38 +1,38 @@
-var PLUGIN_ID = 'tabris-plugin-socket.io';
+class MediaPlayer extends tabris.NativeObject {
+  constructor(properties) {
+    super(properties);
+  }
 
-var SocketIO = tabris.NativeObject.extend('ru.fax1ty.tabris.socket_io.SocketIO');
+  get _nativeType() {
+    return 'ru.fax1ty.tabris.socket_io.SocketIO';
+  }
 
-SocketIO.prototype._listen = function (name, listening) {
-  this._nativeListen(name, listening);
-};
-
-SocketIO.prototype._trigger = function (name, event) {
-  tabris.Widget.prototype._trigger.call(this, name, event);
-};
-
-Object.assign(SocketIO.prototype, {
-  _nativeCreate: function () {
-    tabris.NativeObject.prototype._nativeCreate.apply(this, arguments);
-    return this;
-  },
-
-  emit: function (event, ...args) {
-    if (typeof event != 'string') throw new Error('Invoking "emit" requires a string parameter "event" but received ' + event);
-    if (args === null) this._nativeCall('emit', { event: event });
+  emit(event, ...args) {
+    if (typeof event != 'string') throw new Error('Event argument must be a string!');
     else {
-      if (!Array.isArray(args)) throw new Error('Invoking "emit" requires an array parameter "args" but received ' + args);
-      this._nativeCall('emit', { event: event, args: args });
+      if (!args) this._nativeCall('emit', { event: event });
+      else this._nativeCall('emit', { event: event, args: args });
     }
-  },
+  }
 
-  disconnect: function () {
-    thus._nativeCall('disconnect');
-  },
-
-  dispose: function () {
+  disconnect() {
     this._nativeCall('disconnect');
-    this._dispose();
+  }
+
+  _trigger(name, event) {
+    return super._trigger(name, event);
+  }
+
+  _listen(name, listening) {
+    this._nativeListen(name, listening);
+  }
+}
+
+tabris.NativeObject.defineProperties(MediaPlayer.prototype, {
+  'url': {
+    type: 'string',
+    default: ''
   }
 });
 
-module.exports = SocketIO;
+module.exports = MediaPlayer;
